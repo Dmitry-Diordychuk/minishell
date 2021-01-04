a833040ee80c327657783f2558b051817b98cbe6
PJORGE <pjorge@student.21-school.ru>
2021-01-05 01:06:03 +0300

    TESTEST

--- sources/buildin/echo.c
+++ sources/buildin/echo.c
@@ -3,10 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kdustin <kdustin@student.21-school.ru>     +#+  +:+       +#+        */
+/*   By: che <che@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/17 05:23:04 by kdustin           #+#    #+#             */
-/*   Updated: 2020/12/17 05:23:06 by kdustin          ###   ########.fr       */
+/*   Updated: 2021/01/01 20:58:20 by che              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+#include "minishell.h"
+
+int count_args(char **arg)
+{
+    int n;
+
+    n = 0;
+	while (arg[n])
+		n++;
+	return (n);
+}
+
+int check_flag(char **arg)
+{
+    int i;
+    int flag;
+
+    
+    i = 1;
+    flag = 0;
+    while (arg[i] && ft_strcmp(arg[i], "-n") == 0)
+    {
+        flag = 1;
+        i++;
+    }
+    return(flag);
+}
+
+int ft_echo (char **arg)
+{
+    int i;
+    int flag;
+    int arg_size;
+
+    i = 1;
+    arg_size = count_args(arg);
+    flag = check_flag(arg);
+    if(flag)
+        i++;
+    if(arg_size < 2 && flag == 0)
+        write(1, "\n", 1);
+    if (arg_size)
+    {
+        while (arg[i])
+        {
+            ft_putstr_fd(arg[i], 1);
+            i++;
+            if(arg[i] != NULL && i < arg_size)
+                write(1, " ", 1);
+            if(arg[i] == NULL && flag == 0)
+                write(1, "\n", 1);
+        } 
+    }
+    return (0);
+}
--- sources/buildin/env.c
+++ sources/buildin/env.c
@@ -3,10 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kdustin <kdustin@student.21-school.ru>     +#+  +:+       +#+        */
+/*   By: che <che@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/17 05:25:34 by kdustin           #+#    #+#             */
-/*   Updated: 2020/12/17 05:25:36 by kdustin          ###   ########.fr       */
+/*   Updated: 2021/01/04 23:22:54 by che              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+void	ft_putstrn(char *s)
+{
+	ft_putstr_fd(s, 1);
+	write(1, "\n", 1);
+}
+
+int		ft_env(void)
+{
+	int	i;
+
+	i = 0;
+	while (g_env_vars && g_env_vars[i] != NULL)
+	{
+		ft_putstrn(g_env_vars[i]);
+		i++;
+	}
+	return (1);
+}
